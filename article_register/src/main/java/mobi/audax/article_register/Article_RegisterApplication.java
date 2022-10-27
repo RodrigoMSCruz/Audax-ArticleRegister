0e27e43f95a0b56adc9612004047729d624b2f87
RodrigoMSCruz <rodrigomoraessantacruz@yahoo.com.br>
2022-10-27 16:00:11 -0300

    Correções em Article_RegisterApplication.java

---
+++ article_register/src/main/java/mobi/audax/article_register/Article_RegisterApplication.java
@@ -0,0 +1,13 @@
+package mobi.audax.article_register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Article_RegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Article_RegisterApplication.class, args);
+	}
+
+}
