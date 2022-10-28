1823f096025d4f1326ac466a1bf099e28380ad4a
Rodrigo de Moraes Santa Cruz <85855194+RodrigoMSCruz@users.noreply.github.com>
2022-10-28 11:04:31 -0300

    Delete article_register directory

--- article_register/src/main/java/mobi/audax/article_register/exceptions/ArticlesNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package mobi.audax.article_register.exceptions;
-
-public class ArticlesNotFoundException extends RuntimeException{
-    
-    public ArticlesNotFoundException(){
-        super("Article não encontrado.");
-    }
-    
-}
--- article_register/src/main/java/mobi/audax/article_register/exceptions/UsersNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package mobi.audax.article_register.exceptions;
-
-    public class UsersNotFoundException extends RuntimeException{
-        
-        public UsersNotFoundException(){
-            super("User não encontrado.");
-    }
-    
-}
